804def16feda45474729bf1639b1aef50c3fc044
Fur3k <florianh59320@gmail.com>
2024-03-15 10:57:15 +0100

    Update Exos Florian.c

--- Dossier Florian/Exos Florian/Exos Florian/Exos Florian.c
+++ Dossier Florian/Exos Florian/Exos Florian/Exos Florian.c
@@ -30,24 +30,68 @@ void exo1() {
 */
 void exo2() {
     float* pf1 = (float*) malloc(sizeof(float));
-    if (pf1 == NULL) {
-        exit(1);
-    }
-    *pf1 = 5;
     float* pf2 = (float*) malloc(sizeof(float));
-    if (pf2 == NULL) {
-        exit(1);
-    }
-    float* pf3 = &pf1;
-    pf1 = &pf2;
-    pf2 = &pf3;
-    printf("pf1 : %f, pf2 : %f\n", pf1, pf2);
+    float* pf3 = (float*)malloc(sizeof(float));
+
+    *pf1 = 5.0f;
+    *pf2 = 10.0f;
+
+    *pf3 = *pf1;
+    *pf1 = *pf2;
+    *pf2 = *pf3;
+
+    printf("pf1 : %f, pf2 : %f\n", *pf1, *pf2);
     free(pf1);
     free(pf2);
+    free(pf3);
+}
+
+void tab(float* tableau, int size) {
+    for (int i = 0; i < size; i++) {
+        tableau[i] = 5.0f;
+        printf("%f |", tableau[i]);
+    }
+}
+
+void exo3() {
+    float* tf = (float*)malloc(10 * sizeof(float));
+    for (int i = 0; i < 10; i++) {
+        tf[i] = 0;
+    }
+    tab(tf, 10);
+    free(tf);
+}
+
+void exo4() {
+    float* tf[10] = { 0 };
+    tab(tf, 10);
+}
+
+void exo5() {
+    printf("Choississez la taille du tableau : ");
+    int input; 
+    scanf_s("%d", &input);
+    while (getchar() != '\n');
+    float* tf = (float*)malloc(input * sizeof(float));
+    for (int i = 0; i < input; i++) {
+        printf("[%d] => ", i);
+        float number; 
+        scanf_s("%f", &number);
+        while (getchar() != '\n');
+        tf[i] = number;
+    }
+    for (int i = 0; i < input; i++) {
+        printf("%f |", tf[i]);
+    }
 }
 int main()
 {
     exo1();
     exo2();
+    exo3();
+    printf("\n");
+    exo4();
+    printf("\n");
+    exo5();
     return 0;
 }
