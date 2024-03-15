7fa52047a3632f6fff4db786e0f95d8674996a64
Fur3k <florianh59320@gmail.com>
2024-03-15 11:59:35 +0100

    start demineur

---
+++ Dossier Florian/Démineur/Démineur/Démineur.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Grid {
+    int size;
+} Grid;
+
+typedef struct Tile {
+    int x;
+    int y;
+}Tile;
+
+void create_grid() {
+    Grid g;
+    g.size = 20;
+    int* l = (int*) malloc(g.size * sizeof(int));
+    int* h = (int*)malloc(g.size * sizeof(int));
+    for (int i = 0; i < g.size; i++) {
+        l[i] = 0;
+    }
+}
+
+
+int main()
+{
+    create_grid();
+    return 0;
+}
